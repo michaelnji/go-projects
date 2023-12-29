d3058ca93a54eec55e8ed725d4478a0cdb84b540
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-12-29 16:54:19 +0100

    first commit

---
+++ test/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// import "rsc.io/quote"
+
+func main() {
+    fmt.Println(multiply(1,6))
+}
+
+// multiply takes two integers and returns their product
+func multiply(a int, b int) int {
+    return a * b
+}
